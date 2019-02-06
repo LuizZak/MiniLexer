49116375826162dd4fbcb001642ce537e5335257
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2019-02-06 15:13:08 -0200

    Bumping package to Swift 5.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -41,5 +41,6 @@ let package = Package(
         .testTarget(
             name: "URLParseSampleTests",
             dependencies: ["URLParseSample", "MiniLexer"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
